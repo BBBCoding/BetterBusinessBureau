8d20b6a1ba5631510dfc4493a2e4e4a34c9de489
Brian Carnes <carnes.brian.a@gmail.com>
2022-09-22 09:41:56 -0500

    Added scaffolding for react frontend

---
+++ src/test/java/com/bbb/movies/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbb.movies.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
