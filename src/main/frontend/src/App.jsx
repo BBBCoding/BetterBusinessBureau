9e6b3561b354b621f9bc2e113561cf763001b67a
Benjamin Marks <bjmarks13@yahoo.com>
2022-09-25 10:08:00 -0500

    added code

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './index.css'
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
+import NavBar from "./components/Header/NavBar.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
   return (
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Router exact path="/">
+            Home
+          </Router>
+          <Router path="/genre">
+            Genres
+          </Router>
+          {/*<Router path="/">*/}
+          {/*  Home*/}
+          {/*</Router>*/}
+        </Switch>
+      </Router>
   )
 }
 export default App;
